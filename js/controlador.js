03d4dc8acc5c5b5d9518b882a9fc292e1cc20a8a
Guillermo Roque <roquememo@hotmail.com>
2017-11-02 21:10:39 -0600

    Arreglando enlaces y añadiendo controlador.js

---
+++ js/controlador.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("#select-categorias").html('<option>Todo</option>'+
+		'<option>Ropa</option>'+
+		'<option>Comida y Bebidas </option>'+
+		'<option>Salud y Belleza </option>'+
+		'<option>Deportes </option>'+
+		'<option>Libros y Entretenimiento </option>')
+});
